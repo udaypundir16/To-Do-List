8e3f2d750d00b39de317105bcdbbb59f0c842e6a
Uday Pundir <udayr6588@gmail.com>
2025-12-14 02:42:16 +0530

    added task complete and delete functionality

--- script.js
+++ script.js
@@ -3,13 +3,29 @@ const addBtn = document.getElementById("addBtn");
 const taskList = document.getElementById("taskList");
 
 addBtn.addEventListener("click", () => {
-    const taskText = taskInput.value.trim();
-
+     const taskText = taskInput.value.trim();
     if (taskText === "") return;
+
 
     const li = document.createElement("li");
     li.textContent = taskText;
 
+    li.addEventListener("click", () => {
+        li.classList.toggle("completed");
+    });
+
+
+     const delBtn = document.createElement("span");
+    delBtn.textContent = "❌";
+    delBtn.style.cursor = "pointer";
+
+    delBtn.addEventListener("click", (e) => {
+        e.stopPropagation();
+        li.remove();
+     });
+
+
+    li.appendChild(delBtn);
     taskList.appendChild(li);
     taskInput.value = "";
 });
